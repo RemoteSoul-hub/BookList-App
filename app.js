b03f0ce52c284cb6ba18521ff5e28e487c0cdb4d
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-04-15 23:23:15 +0000

    UI Ready + Styling made using bootstrap materia & font-awesome

---
+++ app.js
@@ -0,0 +1,11 @@
+// Book Class : Represents a Book
+
+// UI CLass : Handle UI Tasks
+
+// Store Class: Handles Storage (Local)
+
+// Event: Display Books
+
+// Event: Add a Book
+
+// Event: Remove a Book
