d0b501a5cf321cb8b737584ada2dc33d77620f19
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-04-16 00:24:57 +0000

    created constructors // descriptions /

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 // Book Class : Represents a Book
 
+class Book {
+    constructor(title, author, summary){
+        this.title = title;
+        this.author = author;
+        this.summary = summary;
+    }
+}
+
 // UI CLass : Handle UI Tasks
+
+class UI {
+    static displayBooks() {
+         const StoredBooks = [
+             {
+                 title: 'Book of Mining',
+                 author: 'Remote Soul',
+                 summary: 'Minecraft 101'
+             }
+         ];
+         const books = StoredBooks;
+
+         books.forEach((book)=> UI.addBookToList(book));
+    }
+
+    static addBookToList(book){
+
+    }
+}
 
 // Store Class: Handles Storage (Local)
 
