15cf5735e3e87e26d7f1b7ce28de7acd6699c325
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-04-17 00:59:31 +0000

    Add book event implemented - Hard coded book included

--- app.js
+++ app.js
@@ -21,18 +21,37 @@ class UI {
          ];
          const books = StoredBooks;
 
-         books.forEach((book)=> UI.addBookToList(book));
+         books.forEach((book)=> UI.addBookToList(book)); 
     }
 
     static addBookToList(book){
-
-    }
+            const list = document.querySelector('#book-list');
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.athor}</td>
+            <td>${book.summary}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete"></a></td>;`
+            list.appendChild(row);
+        }
 }
 
 // Store Class: Handles Storage (Local)
 
 // Event: Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    //Prevent Actual submit 
+    e.preventDefault();
+    // Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const summary = document.querySelector('#summary').value;
+    // Instantiate book
+    const book = new Book(title, author, summary);
+    console.log(book);
+});
 
 // Event: Remove a Book
